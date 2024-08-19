bc372fcf9aca550ba5567c7c205fec6f9dd96552
Danilo Samways <hadeflowing@gmail.com>
2024-08-19 18:16:11 -0300

    feat: tema de cores

--- frontend/src/theme.ts
+++ frontend/src/theme.ts
@@ -3,10 +3,10 @@ import { createTheme } from "@mui/material/styles";
 export const theme = createTheme({
   palette: {
     primary: {
-      main: "#1976d2",
+      main: "#3661b2",
     },
     secondary: {
-      main: "#dc004e",
+      main: "#13a8c2",
     },
   },
   components: {
