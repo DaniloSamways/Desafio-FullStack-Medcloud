ee921db028c7a1c8c6e65fdd4de06b99f96c73b0
Danilo Samways <hadeflowing@gmail.com>
2024-08-24 19:27:47 -0300

    feat: add cors

--- backend/handler.ts
+++ backend/handler.ts
@@ -1,8 +1,11 @@
 import serverless from "serverless-http";
 import express from "express";
 import { router } from "./src/routes/router";
+import cors from "cors";
 
 const app = express();
+
+app.use(cors());
 app.use(express.json());
 app.use(router)
 
